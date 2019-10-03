2a0b7e7f81850514ea762bd0dde82a45a22dbb85
jonathan joly <41478399+joly534@users.noreply.github.com>
2019-10-03 12:42:24 +0200

    refonte du pendu

--- pendu/donnees.py
+++
@@ -1,7 +0,0 @@
-dictionnaire = ["jeu", "cheval", "ordinateur", "salamandre", "pissenlit", "ecran", "imprimante", "velo", "burger",
-                "maison", "python", "tigrou", "boulette", "ventilateur", "xbox", "clavier", "souris", "netflix",
-                "exorciste", "conjuring", "fleur", "avoine", "matrix", "revolution", "ange", "moto", "laguna", "scenic",
-                "charmante", "maman", "papa", "choses", "fin", "figer", "croitre", "maladie", "botte", "cambrioler",
-                "travail"]
-
-fautes = 0
--- pendu/fonctions.py
+++
@@ -1,5 +0,0 @@
-from donnees import *
-from random import choice
-
-def trouver_mot():
-    return choice(dictionnaire)
