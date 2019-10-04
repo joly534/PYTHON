5b9a16191fe2b667cb8fe94cdbb5ae2aa19086c0
jonathan joly <41478399+joly534@users.noreply.github.com>
2019-10-04 19:05:49 +0200

    update

--- spriteetsons.py
+++ spriteetsons.py
@@ -1,9 +1,1 @@
-import pygame, sys, time, random
-from pygame.locals import *
-
-pygame.init()
-mainClock = pygame.time.Clock()
-
-WINDOWWIDTH = 800
-WINDOWHEIGHT = 800
-windowSurface = pygame.display.set_mode(WINDOWWIDTH, WINDOWHEIGHT, 0, 32)
+import pygame
