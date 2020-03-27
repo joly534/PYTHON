8bf12829db96bdbc84142dc842a50a7df5eb32a1
jonathan joly <41478399+joly534@users.noreply.github.com>
2020-03-27 21:35:36 +0100

    update

--- PICORE/main.py
+++ PICORE/main.py
@@ -1,4 +1,13 @@
 import pygame
 
 pygame.init()
+game = True
+pygame.display.set_mode((1280, 720)) 
 
+while game :
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+    
+
+    
