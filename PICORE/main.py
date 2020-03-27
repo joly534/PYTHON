c1f1912f548e42bfb98057a4b6f9a7118cbf954e
jonathan joly <41478399+joly534@users.noreply.github.com>
2020-03-27 21:16:02 +0100

    install pygame and change the game

--- PICORE/main.py
+++ PICORE/main.py
@@ -1,13 +1,4 @@
-from tkinter import *
+import pygame
 
-fenetre = Tk()
+pygame.init()
 
-canvas = Canvas(fenetre, width=1282, height=722)
-canvas.pack()
-
-#image de fond 
-fond = PhotoImage(file="img/fond.gif")
-canvas.create_image(0, 0,anchor='nw',  image=fond)
-
-
-fenetre.mainloop()
